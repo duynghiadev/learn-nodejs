7013460db8be36fe38d8006ced575f39083a2ea5
duynghia-123 <duynghia22302@gmail.com>
2022-10-12 14:55:33 +0700

    Tạo database sử dụng nodejs và sequelize

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const initRoutes = require("./src/routes");
+require("./connectionDatabase");
 
 const app = express();
 app.use(
