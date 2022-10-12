1a54be95e7b3e701f5b4c19b255b8641cbf98457
duynghia-123 <duynghia22302@gmail.com>
2022-10-13 02:33:27 +0700

    Code API register và trả về access token (JWT)

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,8 +1,21 @@
 import db from "../models";
+import bcrypt from "bcryptjs";
 
-export const register = () =>
-  new Promise((resolve, reject) => {
+const hashPassword = (password) =>
+  bcrypt.hashSync(password, bcrypt.genSaltSync(8));
+
+export const register = ({ email, password }) =>
+  new Promise(async (resolve, reject) => {
     try {
+      const response = await db.User.findOrCreate({
+        where: { email },
+        defaults: { email, password: hashPassword(password) },
+      });
+      resolve({
+        err: response[1] ? 0 : 1,
+        mes: response[1] ? "Register is successfully" : "Email is used",
+      });
+
       resolve({
         err: 0,
         mes: "register service",
